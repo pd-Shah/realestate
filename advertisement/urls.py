997851fa95ef3e46b51fb9df2391124b2e295244
pd <pd.shahsafi@gmail.com>
2019-07-15 15:48:41 +0430

    going home

--- advertisement/urls.py
+++ advertisement/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import (
     AdvertisementListView,
+    AboutUsView,
 )
 
+app_name = 'advertisement'
 urlpatterns = [
-    path('', AdvertisementListView.as_view(), name="index")
+    path('', AdvertisementListView.as_view(), name="index"),
+    path('about-us/', AboutUsView.as_view(), name='about_us')
 ]
--- realestate/urls.py
+++ realestate/urls.py
@@ -9,7 +9,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('django.contrib.auth.urls')),
-    path('', include('advertisement.urls')),
+    path('', include('advertisement.urls', namespace='advertisement')),
     ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
