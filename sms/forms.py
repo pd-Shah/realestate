78a2b3f33a9a81986ef9857658e0f63a21e7d299
pd <pd.shahsafi@gmail.com>
2019-08-04 11:17:57 +0430

    sms login conneted to backend

--- sms/forms.py
+++ sms/forms.py
@@ -6,7 +6,7 @@ from django.core.validators import RegexValidator
 
 
 phone_regex = RegexValidator(
-    regex=r'^\+?1?\d{9,15}$',
+    regex=r'^\+?1?\d{11,15}$',
     message="Phone number must be entered in the format: '09226255415'. Up to 15 digits allowed."
 )
 
@@ -15,6 +15,15 @@ class InputPhoneNumber(Form):
     phone_number = forms.CharField(
         validators=[phone_regex],
         max_length=17,
+        label="شماره همراه",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "09226255415",
+                "class": "form-control",
+                "aria-label": "شماره تلفن",
+                "aria-describedby": "basic-addon2",
+            },
+        )
     )
 
 
@@ -22,5 +31,21 @@ class InputCodeForm(Form):
     phone_number = forms.CharField(
         validators=[phone_regex],
         max_length=17,
+        label="شماره همراه",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "09226255415",
+                "class": "form-control",
+                "aria-label": "شماره تلفن",
+                "aria-describedby": "basic-addon2",
+            },
+        )
     )
-    code = forms.CharField()
+    code = forms.CharField(
+        label="کد",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "54321"
+            }
+        )
+    )
