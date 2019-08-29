132f3aacaf763b8039be03ac30d55207fb9623a0
pd <pd.shahsafi@gmail.com>
2019-08-29 12:17:17 +0430

    detailviews connected

--- advertisement/templatetags/extra_tags.py
+++ advertisement/templatetags/extra_tags.py
@@ -6,3 +6,8 @@ register = Library()
 def persian_int(string):
     persianize = dict(zip("0123456789",'۰۱۲۳۴۵۶۷۸۹'))
     return " "+''.join(persianize[digit] if digit in persianize else digit for digit in str(string)) + " تومان"
+
+@register.filter(name='persianize_phone')
+def persian_int(string):
+    persianize = dict(zip("0123456789",'۰۱۲۳۴۵۶۷۸۹'))
+    return " "+''.join(persianize[digit] if digit in persianize else digit for digit in str(string))
